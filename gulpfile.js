cfb34af1a956ffef863bd7897f57bc16a2bfddaa
nemhis <p.nemphis@gmail.com>
2018-10-28 16:32:04 +0300

    Автоматизация: оптимизация картинок

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,9 @@ var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 var csso = require("gulp-csso");
 
+var imagemin = require("gulp-imagemin");
+
+
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
     .pipe(plumber())
@@ -37,3 +40,17 @@ gulp.task("server", function () {
 });
 
 gulp.task("start", gulp.series("css", "server"));
+
+
+/**
+ *  ОПТИМИЗАЦИЯ ИЗОБРАЖЕНИЙ
+ */
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+});

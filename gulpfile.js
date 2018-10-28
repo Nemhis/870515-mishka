f975eb5526271c98bf206fdc31c1fe4ac8975cbc
nemhis <p.nemphis@gmail.com>
2018-10-28 17:25:32 +0300

    Автоматизация: сборка проекта

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require("gulp");
 
 // helpers
 var rename = require("gulp-rename");
+var del = require("del");
 var plumber = require("gulp-plumber");
 var server = require("browser-sync").create();
 
@@ -25,28 +26,12 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
-
-gulp.task("server", function () {
-  server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
-
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
-});
-
-gulp.task("start", gulp.series("css", "server"));
-
 
 /**
  *  Оптимизация изображений
@@ -76,5 +61,50 @@ gulp.task("sprite", function () {
       inlineSvg: true
     }))
     .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
 });
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+  // TODO: здесь будет шаблонизация post html
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("server", function () {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**"
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+// TODO: спрайт пока не вставляется в html, но мы его всё равно забилдим на будущее
+gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+
+gulp.task("start", gulp.series("css", "server"));
